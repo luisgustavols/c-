7fd8b1184cb787bd558b1d950126a4ec9ad06b34
Luis Gustavo <lluisgustavo197@gmail.com>
2026-08-18 19:19:33 -0300

    Add files via upload
    Atividades de c++.

---
+++ atividade.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Bem-vindo ao C++!" << endl;
+    cout << "Meu nome é Luis Gustavo!" << endl;
+    return 0;
+}
---
+++ atividade2.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Bem-vindo ao C++!" << endl;
+    return 0;
+}
